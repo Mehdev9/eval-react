ea736b70a37bc8b61f2171b3fc3d7b7c24bce53d
Mehdev9 <mehdi-belkateb@hotmail.com>
2025-10-30 11:15:10 +0100

    Add Header and Footer components to enhance layout structure

---
+++ app/src/components/Footer.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const Footer = () => {
+  return (
+    <View style={styles.footerContainer}>
+      <Text style={styles.footerText}>© 2025 Gestion des Livres - Tous droits réservés</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  footerContainer: {
+    alignItems: 'center',
+    paddingVertical: 10,
+    backgroundColor: '#333',
+    borderTopWidth: 1,
+    borderTopColor: '#ddd',
+  },
+  footerText: {
+    color: '#fff',
+    fontSize: 14,
+  },
+});
+
+export default Footer;
---
+++ app/src/components/Header.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { useRouter } from 'expo-router';
+
+const Header = () => {
+  const router = useRouter();
+
+  return (
+    <View style={styles.headerContainer}>
+      <Text style={styles.headerTitle}>Gestion des Livres</Text>
+
+      <View style={styles.navContainer}>
+        <TouchableOpacity onPress={() => router.push('./')}>
+          <Text style={styles.navLink}>Accueil</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => router.push('/src/screens/BookListScreen')}>
+          <Text style={styles.navLink}>Livres</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => router.push('/src/screens/AddEditBookScreen')}>
+          <Text style={styles.navLink}>Ajouter</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  headerContainer: {
+    flexDirection: 'column',
+    alignItems: 'center',
+    backgroundColor: '#4CAF50', // Couleur de fond du header
+    padding: 15,
+    height: 80,
+    justifyContent: 'center',
+  },
+  headerTitle: {
+    fontSize: 20,
+    color: '#fff',
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  navContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '60%',
+  },
+  navLink: {
+    color: '#fff',
+    fontSize: 16,
+    marginHorizontal: 10,
+    textDecorationLine: 'underline',
+  },
+});
+
+export default Header;
